0933b4148dd1b06ad409a2aa21f79e0a8d07557a
OnurCelebi <Onur.Celebi@7im.co.uk>
2023-03-05 23:39:41 +0000

    Grid coordination validation added. Validated value for any coordinate to prevent create bigger than 50.

--- MartianRobots/Program.cs
+++ MartianRobots/Program.cs
@@ -1,11 +1,31 @@
 ﻿
+using MartianRobots;
+
 class Program
 {
+    public static int GridCoordinateXMax = 50; // can be move to config file.
+    public static int GridCoordinateYMax = 50;
+    public static int GridCoordinateXMin = 0;
+    public static int GridCoordinateYMin = 0;
 
     static void Main(string[] args)
     {
+
+        bool isGridValid;
+        var worldCoordinates = String.Empty;
+
+
         var marsGrid = Console.ReadLine();
         var grid = Array.ConvertAll(marsGrid.Split(' '), int.Parse);
+
+        do
+        {
+            Console.WriteLine("TYPE MARS WORLD COORDINATES. ( Input example: 15 50. The maximum value for coordinates are  (" + GridCoordinateYMax + "," + GridCoordinateYMax + "))");
+            worldCoordinates = Console.ReadLine().Trim();
+            isGridValid = ValidationHelper.ValidateGridCoordinate(worldCoordinates);
+        }
+        while (!isGridValid);
+
 
         while (true)
         {
@@ -21,4 +41,5 @@ class Program
             Console.WriteLine("Result: " + result.x + " " + result.y + " " + result.orientation);
         }
     }
+    
 }
---
+++ MartianRobots/ValidationHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianRobots
+{
+    internal class ValidationHelper
+    {
+        public static int GridCoordinateXMax = 50;
+        public static int GridCoordinateYMax = 50;
+        public static int GridCoordinateXMin = 0;
+        public static int GridCoordinateYMin = 0;
+
+        public static bool ValidateGridCoordinate(string gridCoordinates)
+        {
+
+            if (String.IsNullOrEmpty(gridCoordinates))
+            {
+                return false;
+            }
+
+            var gridCoordinatesTrimmed = gridCoordinates.Trim().Split(" "); 
+
+            if (gridCoordinatesTrimmed.Length == 2)
+            {
+                bool isXNumber = int.TryParse(gridCoordinatesTrimmed[0], out int coordinateX);
+                bool isYNumber = int.TryParse(gridCoordinatesTrimmed[1], out int coordinateY);
+
+                return isXNumber && isYNumber && coordinateY > GridCoordinateYMin && coordinateY <= GridCoordinateYMax;
+            }
+
+            return false;
+        }
+    }
+}
