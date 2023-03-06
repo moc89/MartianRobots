30b2be34d5c6807880e71de9f93a3507dea3f627
OnurCelebi <Onur.Celebi@7im.co.uk>
2023-03-06 00:56:21 +0000

    Robot have been lost successfully.

--- MartianRobotsTests/MartianRobotsUnitTests.cs
+++ MartianRobotsTests/MartianRobotsUnitTests.cs
@@ -26,5 +26,27 @@ namespace MartianRobotsTests
             // Assert
             Assert.Equal(expectedOutput, combinedResult);
         }
+
+
+        [Theory]
+        [InlineData("5 3", "3 2 N", "FRRFLLFFRRFLL", "33N LOST")]
+        public void RobotHaveBeingLost(string gridInput, string robotInput, string instruction, string expectedOutput)
+        {
+            // Arrange
+            var lostRobots = new List<(int, int, string)>();
+            var robotPositions = robotInput.Split(" ");
+            var grid = Array.ConvertAll(gridInput.Split(' '), int.Parse);
+
+            IRobot robot = new Robot(int.Parse(robotPositions[0]), int.Parse(robotPositions[1]), robotPositions[2], grid);
+
+            // Act
+            var result = robot.Execute(instruction);
+            string lostMessage = result.isRobotLost ? "LOST" : string.Empty;
+            var combinedResult = result.x.ToString() + result.y.ToString() + result.orientation + " " + lostMessage;
+
+            // Assert
+            Assert.Equal(expectedOutput, combinedResult);
+        }
+
     }
 }
